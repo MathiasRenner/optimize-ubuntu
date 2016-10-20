36d71e0ee85a8d9497f0d82462f31a9744e20426
Mathias Renner <MathiasRenner@users.noreply.github.com>
2016-10-20 14:03:32 +0200

    Extend script, section 'Software Saeuberung' is completed

--- Script.sh
+++ Script.sh
@@ -15,8 +15,29 @@ echo 'In der Dash-Suche, deaktiviere Ergebnisse aus dem Internet'
 #http://askubuntu.com/questions/271408/how-to-hide-unity-dash-more-suggestions-not-unity-lens-shopping
 
 
-
 ### Software Update ###
+
+echo 'Installiere/Aktualisiere Java'
+sudo apt-get install default-jre
+# test mit java -version
+
+echo 'Update aller installierten Anwendungen'
+sudo apt-get update && sudo apt-get upgrade
+
+echo 'Installiere/Aktualisiere shutter'
+sudo apt-get install shutter
+
+echo 'Installiere/Aktualisiere Adobe Flash Player'
+sudo apt-get install flashplugin-installer
+
+echo 'Installiere/Aktualisiere TeamViewer'
+sudo apt-get install teamViewer
+
+echo 'Installiere/Aktualisiere Firefox'
+sudo apt-get install teamViewer
+
+echo 'Installiere Firefox Add-Ons'
+#http://askubuntu.com/questions/73474/how-to-install-firefox-addon-from-command-line-in-scripts
 
 
 
