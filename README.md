222d48a463543b1903a62db93c24156518dd33a3
Mathias Renner <mail@mathias-renner.com>
2017-03-20 16:35:47 +0100

    -- Almost complete rewrite --

---
+++ README.md
@@ -0,0 +1,76 @@
+# Software-Optimierung für Ubuntu
+
+Die "Software-Optimierung" für Computer entspricht etwa dem TÜV beim Auto. Die Software von Computern kann damit so optimiert werden kann, dass
+
+  - die Eintrittswahrscheinlichkeit von Fehlern im Betrieb langfristig minimiert wird,
+  - die Sicherheit und der Schutz der Privatsphäre maximiert wird,
+  - Die Usability eines Computers maximiert wird, da z.B. alle notwendigen Anwendungen für möglichst alle Aufgaben des Nutzers installiert werden.
+
+Die Software-Optimierung kann jederzeit auf neuen wie auch auf gebrauchten Computern durchgeführt werden. Eine regelmäßige Anwendung wird empfohlen.
+
+
+## Ablauf
+
+**Achtung! Eine nicht sachgemäße Anwendung der Software-Optimierung kann Probleme hervorrufen! Verwendung auf eigene Gefahr!**
+
+
+### Absprachen im Voraus
+
+- Notwendigikeit einer Datenmigration?
+- Benutzer und Login-Passwort abfragen
+- Verwendete Software abfragen
+- Sicherungsniveau: Festplatten-Verschlüsselung, Passwortspeicher, VPN, Browser Fingerprint etc.
+
+
+### Erstellung einer Datensicherung als RAW-Diskimage
+
+
+### Hardware ggf. pneumatisch entstauben und feucht reinigen
+
+
+### Prüfung der Funktionalität
+
+- Netzwerk: WLAN und Ethernet
+- USB-Slots
+- Kartenleser
+- Webcam
+- Touchpad
+- CD-Laufwerk
+- Funktionstasten
+
+
+### Software Säuberung (automatisiert)
+
+- Ungenutze Software oder Plugins in Software entfernen
+- Cleanup Software installieren
+
+
+### Software Update (automatisiert)
+
+- Update aller installierten Anwendungen
+- Installiere/Aktualisiere sinnvolle Anwendungen (Browser, Java etc.) inkl. sinnvolle Browser Plugins,
+- Automatische Sicherheits- und Standard-Updates auf nicht-interaktiv setzen
+
+
+### Optimierung der Einstellungen (automatisiert)
+
+- Optimiere relevante Software-Einstellungen auf Geschwindigkeit, Sicherheit und Privatsphäre
+
+
+### Abschließende Prüfung (manuell)
+
+- Ggf. weitere, unnütze Software entfernen
+- Ggf. Cleanup software verwenden
+- Auflösung des Monitors anpassen
+- Energieoptionen optimieren
+- Netzwerkeinstellungen prüfen
+- Druckereinstellungen prüfen
+- Adobe Flash Player testen
+- Benutzerkonten-Check
+- Ggf. Defragmentierung: „sudo e4defrag -c /“, wenn nötig, dann run „sudo e4defrag /“ http://wiki.ubuntuusers.de/Defragmentierung
+- Autostartobjekte prüfen
+- Verknüpfungen im Anwendungsmenü auf relevante Software reduzieren
+- Infobereich prüfen, Ggf. Sprachsymbol aus Menüleiste entfernen
+- Neustarten, dabei BIOS-Einstellungen prüfen
+- Sicherung durchführen und prüfen
+- Gerät zur Ausliefung an den Kunden in Stand-by versetzen
---
+++ software-optimierung.sh
@@ -0,0 +1,97 @@
+#! /usr/bin/env bash
+
+# USE THIS SCRIPT AT YOUR OWN RISK #
+
+# Terminate on any error
+set -e
+
+
+#################################################
+### Software Säuberung (automatisiert)
+#################################################
+
+echo "----> Cleanup Pakete automatisch aufräumen"
+sudo apt-get autoclean autoremove
+
+printf "\n----> Alte Kernel in Boot-Partition säubern"
+sudo dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+
+printf "\n----> Install FSlint"
+sudo apt-get install -y fslint
+
+printf "\n----> Online-Ergebnisse in Dash-Suche deaktivieren (u.a. Amazon)"
+# https://askubuntu.com/questions/450398/how-to-remove-amazon
+sudo apt-get remove -y unity-webapps-common
+
+
+
+#################################################
+### Software Update (automatisiert)
+#################################################
+
+printf "\n----> Update aller installierten Anwendungen"
+sudo apt-get update && sudo apt-get upgrade -y
+
+printf "\n----> Installiere/Aktualisiere Java"
+sudo apt-get install -y default-jre
+# test mit java -version
+
+printf "\n----> Installiere/Aktualisiere shutter"
+sudo apt-get install -y shutter
+
+# add shortcut with "shutter -f" und autom. speichern
+# http://shutter-project.org/faq-help/set-shutter-as-the-default-screenshot-tool/#gnom
+
+printf "\n----> Installiere/Aktualisiere Adobe Flash Player"
+sudo apt-get install -y flashplugin-installer
+
+printf "\n----> Installiere/Aktualisiere TeamViewer"
+sudo apt-get install -y teamViewer
+
+printf "\n----> Installiere/Aktualisiere Clipboard Manager"
+sudo apt-get install -y copyq
+
+
+printf "\n----> Installiere/Aktualisiere Firefox"
+sudo apt-get install -y firefox
+
+printf "\n----> Installiere/Aktualisiere Chromium"
+sudo apt-get install -y chromium-browser
+
+printf "\n----> Installiere Firefox Add-Ons"
+# uBlock, PrivacyBadger
+#http://askubuntu.com/questions/73474/how-to-install-firefox-addon-from-command-line-in-scripts
+
+#ggf. Plugins entfernen
+
+# Required to play DVs
+# http://howtoubuntu.org/how-to-play-a-dvd-in-ubuntu
+# https://wiki.ubuntuusers.de/DVD-Wiedergabe/
+sudo apt-get install -y libdvdcss libdvdread4 libdvdnav4 libdvd-pkg
+sudo dpkg-reconfigure libdvd-pkg
+
+
+
+#################################################
+### Optimierung der Einstellungen (automatisiert)
+#################################################
+
+# firefox: Beim start leere Seite anzeigen, Startpage oder Ecosia als Suchmaschine anpassen und als Startseite setzen, Immer den privaten Modus verwenden, Cookies von Drittanbietern nie akzeptieren, keinerelei Passwörter speichern, (Erweitert → Datenübermittlung) Keine Daten senden
+
+# Automatische Sicherheits- und Standard-Updates auf nicht-interaktiv setzen
+
+# Bluethooth deaktivieren (/etc/bluetooth/main.conf, RememberPowered und InitiallyPowered →  false)
+
+#In Systemeinstellungen, Senden von Berichten an Canonical unterbinden (setze Parameter auf "0" in /etc/default/apport)
+
+
+printf "\n----> Abschließend Pakete automatisch aufräumen"
+sudo apt-get autoclean autoremove
+
+#remove effects
+sudo apt-get install compizconfig-settings-manager
+# open it via `ccsm`, go to "effects" disable checkbox
+
+printf "\n----> Finished.\n"
+
+### Abschließende Prüfung (manuell)
