804c46f23f9f572f4014565d0154a9f80ce5cc16
Mathias Renner <MathiasRenner@users.noreply.github.com>
2017-03-26 23:33:04 +0200

    Add license

--- automated-tasks.sh
+++ automated-tasks.sh
@@ -1,6 +1,21 @@
 #! /usr/bin/env bash
 
-# USE THIS SCRIPT AT YOUR OWN RISK #
+# This software is licensed unter the GPLv3.
+#
+# Copyright (C) 2017 Mathias Renner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See http://www.gnu.org/licenses/ fore more information.
+
 
 # Terminate on any error
 set -e
